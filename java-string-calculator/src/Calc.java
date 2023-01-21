a1ed7237c21f2909c9fcc9b58126fb842e8a2a7f
ahn-sj <jaesa5221@gmail.com>
2023-01-21 19:52:32 +0900

    [문자열 계산기] string-calculator

--- java-string-calculator/src/Calc.java
+++ java-string-calculator/src/Calc.java
@@ -1,16 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Calc {
-    public static void main(String[] args) {
-        String[] operators = {"+", "-", "*", "/"};
-        String given = "3+4";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine().replace(" ", "");
 
-        for (int i = 0; i < given.length(); i++) {
-            for (int j = 0; j < operators.length; j++) {
-                boolean isContain = String.valueOf(given.charAt(i)).contains(operators[j]);
+        Queue<Character> operators = divideOperator(input);
+        String[] operands = divideOperand(input);
 
-                if(isContain) {
-//                    operands[0] = given.ch
-                }
+        int rst = Integer.parseInt(operands[0]);
+
+        for (int i = 1; i < operands.length; i++) {
+            Character operator = operators.poll();
+            int operand = Integer.parseInt(operands[i]);
+
+            calculator(rst, operator, operand);
+        }
+    }
+
+    private static Queue<Character> divideOperator(String input) {
+        Queue<Character> queue = new LinkedList<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if(isValidRange(input, i, c)) {
+                queue.offer(input.charAt(i));
             }
+        }
+        return queue;
+    }
+
+    private static String[] divideOperand(String input) {
+        return input.split("\\+|-|\\*|/");
+    }
+
+    private static int calculator(int rst, Character operator, int operand) {
+        if(operator == '+') rst += operand;
+        if(operator == '-') rst -= operand;
+        if(operator == '*') rst *= operand;
+        if(operator == '/') {
+            isDivideZero(operand);
+            rst /= operand;
+        }
+        System.out.println("rst = " + rst);
+        return rst;
+    }
+
+    private static boolean isValidRange(String input, int i, char c) {
+        return !(c - '0' >= 0 && input.charAt(i) - '0' < 10);
+    }
+
+    private static void isDivideZero(int operand) {
+        if(operand == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
         }
     }
 }
